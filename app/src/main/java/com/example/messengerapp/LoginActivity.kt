e53b60857485b9017504fd0b08fbd86afc8189e2
hasham7861 <hasham.alam@hotmail.com>
2019-06-03 22:46:25 -0400

    Able to register and authentic to firebase and updated the app ui

--- app/src/main/java/com/example/messengerapp/LoginActivity.kt
+++ app/src/main/java/com/example/messengerapp/LoginActivity.kt
@@ -3,8 +3,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity: AppCompatActivity(){
 
@@ -13,22 +14,44 @@ class LoginActivity: AppCompatActivity(){
         setContentView(R.layout.activity_login);
 
         login_button_login.setOnClickListener {
-            val email = email_edittext_login.text.toString()
-            val password = password_edittext_login.text.toString()
 
-            Log.d("LoginActivity","Email: $email")
-            Log.d("LoginActivity","Password: $password")
         }
 
 
         back_to_register_textview.setOnClickListener {
 
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+            finish()
 
         }
 
+    }
 
+    private fun performLogin(){
+
+        val email = email_edittext_login.text.toString()
+        val password = password_edittext_login.text.toString()
+
+        Log.d("LoginActivity","Email: $email")
+        Log.d("LoginActivity","Password: $password")
+
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this,"Please enter text in email with password", Toast.LENGTH_SHORT).show()
+            return
+        }
+        // Authentication to create user with given info
+        FirebaseAuth.getInstance()
+            .signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                // else if successful
+                Log.d("Login","Successfully logged in : ${it.result?.user?.uid}")
+            }
+            .addOnFailureListener{
+                Log.d("Login","Can't login in : ${it.message}")
+            }
 
     }
 }
