c0064f6ed9da29cc475cd5b19cd984d5990bbb3e
hasham7861 <hasham.alam@hotmail.com>
2019-07-03 07:56:46 -0400

    Intial Commit

---
+++ app/src/main/java/com/example/secretmessagingapp/LatestMessagesActivity.kt
@@ -0,0 +1,57 @@
+package com.example.secretmessagingapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+
+
+class LatestMessagesActivity : AppCompatActivity (){
+
+    override fun onCreate(savedInstantState: Bundle?){
+        super.onCreate(savedInstantState)
+        setContentView(R.layout.activity_latest_messages)
+        verifyLogin()
+
+    }
+
+    private fun verifyLogin(){
+
+        val uid = FirebaseAuth.getInstance().uid
+        // Meaning user is not logged in
+        if(uid == null){
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.menu_new_message -> {
+                val intent = Intent(this, NewMessageActivity::class.java)
+                Log.d("New Message", "Clicked")
+                startActivity(intent)
+            }
+
+            R.id.menu_sign_out -> {
+                FirebaseAuth.getInstance().signOut()
+
+                val intent = Intent (this, LoginActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
---
+++ app/src/main/java/com/example/secretmessagingapp/LoginActivity.kt
@@ -0,0 +1,61 @@
+package com.example.secretmessagingapp
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            performLogin()
+        }
+
+        back_to_register_textview.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun performLogin(){
+
+        val email = email_edittext_login.text.toString()
+        val password = password_edittext_login.text.toString()
+
+        Log.d("LoginActivity","Email: $email")
+        Log.d("LoginActivity","Password: $password")
+
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this,"Please enter text in email with password", Toast.LENGTH_SHORT).show()
+            return
+        }
+        // Authentication to create user with given info
+        FirebaseAuth.getInstance()
+            .signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                // else if successful
+                val intent = Intent(this, LatestMessagesActivity::class.java)
+                // clears all the old activities from the context
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+
+                Toast.makeText(this,"Successfully logged in",Toast.LENGTH_SHORT).show()
+                Log.d("Login","Successfully logged in : ${it.result?.user?.uid}")
+            }
+            .addOnFailureListener{
+                Log.d("Login","Can't login in : ${it.message}")
+                Toast.makeText(this,"Incorrect email / password",Toast.LENGTH_SHORT).show()
+            }
+
+    }
+}
